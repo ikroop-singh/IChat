eb1dc717bb06f39eef4f91b5344d4d503ccf44e3
ikroop-singh <ikroopsingh1234@gmail.com>
2024-10-25 00:15:14 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,17 @@
 const express=require('express')
 const dotenv=require('dotenv')
 const cookieParser=require('cookie-parser')
+const path=require('path')
 
 const authRoutes=require('./routes/auth.routes')
 const messageRoutes=require('./routes/message.routes.js')
 const userRoutes=require('./routes/user.routes')
 const {server,io,app}=require('./socket/socket.js')
 
+
 const connectToMongo =require('./db/connection')
 
-dotenv.config({path:'../.env'})
+dotenv.config()
 const PORT=process.env.PORT ||5000
 // const app=express()
 
@@ -19,6 +21,11 @@ app.use(cookieParser())
 app.use('/api/auth',authRoutes)
 app.use('/api/messages',messageRoutes)
 app.use('/api/users',userRoutes)
+app.use(express.static(path.join(__dirname,'../frontend/dist')))
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+});
 
 server.listen(PORT,()=>{    
     connectToMongo()
